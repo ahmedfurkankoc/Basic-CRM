f39588344f3aa6be9180c14085835782ea41781e
Ahmed Furkan Koc <ahmedfurkankoc@gmail.com>
2024-05-17 02:45:10 +0300

    first commit

---
+++ crm/webapp/forms.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.forms.widgets import PasswordInput, TextInput
+from django import forms
+
+
+# Register a user
+
+class CreateUserForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+
+
+# Login a user
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
---
+++ crm/webapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm, LoginForm
+
+# - Home
+def home(request):
+    return render(request, 'webapp/index.html')
+
+# - Register a user
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            # return redirect('')
+    
+    context = {'form': form}
+
+    return render(request, 'webapp/register.html', context=context)
